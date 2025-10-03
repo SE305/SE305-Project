fae1d6f555cb48625dd5611fe9b10b17b48f841c
H5Anan <husaain4497@gmail.com>
2025-10-03 14:27:29 +0300

    add books

---
+++ src/Book.java
@@ -0,0 +1,23 @@
+public class Book {
+    private String id;
+    private String title;
+    private String author;
+    private int year;
+
+    public Book(String id, String title, String author, int year) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.year = year;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + id + "] " + title + " by " + author + " (" + year + ")";
+    }
+}
+
