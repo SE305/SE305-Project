bc731657298bd5c3daa25370704cdfa0a8af2936
M-Alradhi <mariamalradhi1@gmail.com>
2025-10-14 23:15:23 +0300

    Phase III: Barrow System with Source Code and JUnit Tests

---
+++ src/test/Book1.java
@@ -0,0 +1,30 @@
+package test;
+
+// ================== (Entity Layer) ==================
+
+// Represents a book with its title and availability
+public class Book1 {
+    private String title;
+    private boolean isAvailable;
+
+    // Constructor to make a new book (default available = true)
+    public Book1(String title) {
+        this.title = title;
+        this.isAvailable = true;
+    }
+
+    // Getter for title
+    public String getTitle() {
+        return title;
+    }
+
+    // Getter to check availability
+    public boolean getStatus() {
+        return isAvailable;
+    }
+
+    // Mark the book as borrowed
+    public void markAsBorrowed() {
+        this.isAvailable = false;
+    }
+}
