0fd3a859acbbf99573691ca1f938bd435c000539
M-Alradhi <mariamalradhi1@gmail.com>
2025-11-04 22:19:42 +0300

    Reorganize: Move all project files to BarrowABook folder

---
+++ BarrowABook/src/randoop/Book1_Randoop.java
@@ -0,0 +1,11 @@
+package randoop;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ Book1_Randoop0.class })
+public class Book1_Randoop {
+}
+
+
