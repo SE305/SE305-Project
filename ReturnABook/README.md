01b0c64a4e2e6969d94f1bd2cceb946d2a57496e
Rayan Mahmood <rayanmahmood56@gmail.com>
2025-11-05 13:08:32 +0300

    Add files from Rayan-Mahmood branch to main/ReturnABook

--- ReturnABook/README.md
+++
@@ -1,5 +0,0 @@
-Software Engineering Project – ITSE305
-University of Bahrain – College of IT
-
-Project Title
-[Online Library Management System]
--- ReturnABook/SearchBook/src/test/randooptests/Book_Randoop.java
+++
@@ -1,9 +0,0 @@
-package test.randooptests;
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
-
-@RunWith(Suite.class)
-@Suite.SuiteClasses({ Book_Randoop0.class })
-public class Book_Randoop {
-}
-
