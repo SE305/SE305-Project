5ca4d2efc4521ad465c19fd41b4e28554d01a683
SE305 <maryamalradhi3@gmail.com>
2025-11-30 17:02:15 +0300

    Update Book.java

--- src/Book.java
+++ src/Book.java
@@ -1,7 +1,3 @@
-✅ Excellent class design - perfect encapsulation
-✅ Clean and readable toString() method
-✅ Good constructor implementation
-    
 public class Book {
     private String id;
     private String title;
@@ -9,12 +5,6 @@ public class Book {
     private int year;
 
     public Book(String id, String title, String author, int year) {
-        if(id==null|| id.trim().isEmpty()){
-           throw new IllegeArgumentException("Book ID cannot by empty");
-        }
-
-        if (year<1000 || year>2025){
-            throw new IllegeArgumentException("Invalid publication year:"+year);
         this.id = id;
         this.title = title;
         this.author = author;
