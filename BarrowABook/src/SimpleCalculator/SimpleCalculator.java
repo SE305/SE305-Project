f9b784ef1bbece8f648dbab46560094041142db9
M-Alradhi <mariamalradhi1@gmail.com>
2025-11-30 19:31:28 +0300

    Update SimpleCalculator.java
    Done

--- BarrowABook/src/SimpleCalculator/SimpleCalculator.java
+++ BarrowABook/src/SimpleCalculator/SimpleCalculator.java
@@ -15,6 +15,9 @@ public class SimpleCalculator {
     }
 
     public int divide(int a, int b) {
+        if(b==0){
+            throw new IllegalArgumentException("Cannot divide by zero");
+        }
         return a / b;
     }
 }
