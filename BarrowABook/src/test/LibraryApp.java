84a0b3ea5aacbecabd1fa6056b7431b6c0fd4b83
M-Alradhi <mariamalradhi1@gmail.com>
2025-11-30 19:57:29 +0300

    Update LibraryApp.java
    Done

--- BarrowABook/src/test/LibraryApp.java
+++ BarrowABook/src/test/LibraryApp.java
@@ -1,33 +1,34 @@
 //Hard to test
 package test;
 
+import java.io.InputStream;
 import java.util.Scanner;
 
 // ================== (Presentation Layer) ==================
 // Handles interaction with the user
 public class LibraryApp {
     public static void main(String[] args) {
-        startLibrarySystem();
+        startLibrarySystem(System.in);
     }
 
     // Method to start the library system (for testing purposes)
-    public static void startLibrarySystem() {
+    public static void startLibrarySystem(InputStream input) {
         // This method contains the interactive loop - we can't test it in unit tests
         LibraryService library = new LibraryService();
-        Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(input);
 
         System.out.println("===== Welcome to My Library System =====");
 
         while (true) {
             System.out.print("Enter a book title to borrow, or type 'exit' to quit:\n ");
-            String input = sc.nextLine();
+            String userInput = sc.nextLine();
 
-            if (input.equalsIgnoreCase("exit")) {
+            if (userInput.equalsIgnoreCase("exit")) {
                 System.out.println("Thanks for visiting! See you again.");
                 break;
             }
 
-            library.borrowBook(input);
+            library.borrowBook(userInput);
         }
         sc.close();
     }
