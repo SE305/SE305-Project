a9a61203d0edad939557cac74a5666ced1084347
M-Alradhi <mariamalradhi1@gmail.com>
2025-11-30 15:20:46 +0300

    Update SimpleCalculator.java

--- src/SimpleCalculator/SimpleCalculator.java
+++ src/SimpleCalculator/SimpleCalculator.java
@@ -19,6 +19,9 @@ public class SimpleCalculator {
     }
 
     public int divide(int a, int b) {
+        if (b == 0) {
+            throw new ArithmeticException("Division by zero is not allowed");
+        }
         return a / b;
     }
 }
